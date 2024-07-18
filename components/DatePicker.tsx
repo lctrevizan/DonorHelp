8f6ef19d8d1273ae1f4b84965099be6d11978df0
lutvzn <106083845+lutvzn@users.noreply.github.com>
2024-07-18 12:13:53 -0300

    Simmplifed the date picker

--- components/DatePicker.tsx
+++ components/DatePicker.tsx
@@ -24,71 +24,43 @@ const DatePicker: React.FC<DatePickerProps> = ({
   setAno,
   anoAtual,
 }) => {
+  const renderPicker = (
+    value: string,
+    setValue: (value: string) => void,
+    label: string,
+    items: number | string[]
+  ) => (
+    <View style={estilos.pickerWrapper}>
+      <Picker
+        selectedValue={value}
+        style={estilos.picker}
+        onValueChange={setValue}
+      >
+        <Picker.Item label={label} value="" />
+        {Array.isArray(items)
+          ? items.map((item) => (
+              <Picker.Item key={item} label={`${item}`} value={`${item}`} />
+            ))
+          : [...Array(items).keys()].map((i) => (
+              <Picker.Item key={i} label={`${i + 1}`} value={`${i + 1}`} />
+            ))}
+      </Picker>
+      <Icon
+        name="arrow-drop-down"
+        size={24}
+        color="white"
+        style={estilos.pickerIcon}
+      />
+    </View>
+  );
+
+  const years = Array.from({ length: 6 }, (_, i) => (anoAtual + i).toString());
+
   return (
     <View style={estilos.containerPicker}>
-      {/* Picker para selecionar o dia */}
-      <View style={estilos.pickerWrapper}>
-        <Picker
-          selectedValue={dia}
-          style={estilos.picker}
-          onValueChange={(itemValue) => setDia(itemValue)}
-        >
-          <Picker.Item label="Dia" value="" />
-          {[...Array(31).keys()].map((i) => (
-            <Picker.Item key={i} label={`${i + 1}`} value={`${i + 1}`} />
-          ))}
-        </Picker>
-        <Icon
-          name="arrow-drop-down"
-          size={24}
-          color="white"
-          style={estilos.pickerIcon}
-        />
-      </View>
-
-      {/* Picker para selecionar o mês */}
-      <View style={estilos.pickerWrapper}>
-        <Picker
-          selectedValue={mes}
-          style={estilos.picker}
-          onValueChange={(itemValue) => setMes(itemValue)}
-        >
-          <Picker.Item label="Mês" value="" />
-          {[...Array(12).keys()].map((i) => (
-            <Picker.Item key={i} label={`${i + 1}`} value={`${i + 1}`} />
-          ))}
-        </Picker>
-        <Icon
-          name="arrow-drop-down"
-          size={24}
-          color="white"
-          style={estilos.pickerIcon}
-        />
-      </View>
-
-      {/* Picker para selecionar o ano */}
-      <View style={estilos.pickerWrapper}>
-        <Picker
-          selectedValue={ano}
-          style={estilos.picker}
-          onValueChange={(itemValue) => setAno(itemValue)}
-        >
-          <Picker.Item label="Ano" value="" />
-          {[...Array(10).keys()].map((i) => (
-            <Picker.Item
-              key={i}
-              label={`${anoAtual + i}`}
-              value={`${anoAtual + i}`}
-            />
-          ))}
-        </Picker>
-        <Icon
-          name="arrow-drop-down"
-          size={24}
-          color="white"
-          style={estilos.pickerIcon}
-        />
-      </View>
+      {renderPicker(dia, setDia, "Dia", 31)}
+      {renderPicker(mes, setMes, "Mês", 12)}
+      {renderPicker(ano, setAno, "Ano", years)}
     </View>
   );
 };
@@ -107,8 +79,6 @@ const estilos = StyleSheet.create({
   picker: {
     height: 50,
     width: "100%",
-    marginHorizontal: 0,
-    textAlign: "center",
     color: "white",
     backgroundColor: "#212121",
   },
